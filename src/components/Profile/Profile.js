da2b2199a7e5a46d4e24470d714d9419ec554b8b
lukecarnevale <lukecarnevale19@gmail.com>
2021-02-06 11:17:19 -0500

    profile photo and styling added

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,9 +1,25 @@
+import { Typography } from '@material-ui/core'
 import React from 'react'
+
+import Luke from '../../assets/images/Luke.jpg'
+import './Profile.css'
 
 const Profile = () => {
   return (
-    <div>
-            This is the profile component
+    <div className='profile container_shadow '>
+      <div className='profile_name'>
+        <Typography className='name'>Luke</Typography>
+        <Typography className='title'>Luke</Typography>
+      </div>
+      <figure className='profile_image'>
+        <img src={Luke} alt='' />
+      </figure>
+
+      <div className='profile_information'>
+          Insert Timeline
+        <br />
+        <button>My button</button>
+      </div>
     </div>
   )
 }
