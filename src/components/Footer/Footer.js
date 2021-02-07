39d9140407cc1aded4c365078fff776388f39f45
lukecarnevale <lukecarnevale19@gmail.com>
2021-02-07 00:30:56 -0500

    footer added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,20 @@
+import { Typography } from '@material-ui/core'
 import React from 'react'
+import './Footer.css'
+import resumeData from '../../utils/resumeData'
 
 const Footer = () => {
   return (
-    <div>
-            This is the footer component
+    <div className='footer'>
+      <div className='footer_left'>
+        <Typography className='footer_name'>{resumeData.name}</Typography>
+      </div>
+      <div className='footer_right'>
+        <Typography className='footer_copyright'>
+        Designed and Developed by <a href='https://www.linkedin.com/in/lucas-carnevale/'>Luke Carnevale</a>
+          <div>&#169; <span>{(new Date().getFullYear())}</span> All rights reserved.</div>
+        </Typography>
+      </div>
     </div>
   )
 }
