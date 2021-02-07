6cad606ba21e031674bca6b81a111dc8b2f3ef9a
lukecarnevale <lukecarnevale19@gmail.com>
2021-02-07 12:54:03 -0500

    about me section inital styling

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
   return (
     <div className='footer'>
       <div className='footer_left'>
-        <Typography className='footer_name'>{resumeData.name}</Typography>
+        <Typography className='footer_name'>{resumeData.name}.</Typography>
       </div>
       <div className='footer_right'>
         <Typography className='footer_copyright'>
--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -1,10 +1,28 @@
 import React from 'react'
+import './Resume.css'
+import { Grid, Typography } from '@material-ui/core'
+import resumeData from '../../utils/resumeData'
 
 const Resume = () => {
   return (
-    <div>
-            Resume page
-    </div>
+    <>
+      {/* About Me */}
+      <Grid container className='section pb_45'>
+        <Grid item className='section_title mb_30'>
+          <span />
+          <h6 className='section_title_text'>About Me</h6>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant='body2' className='aboutme_text'>{resumeData.about}</Typography>
+        </Grid>
+      </Grid>
+      {/* Education and Experience */}
+      <Grid container className='section' />
+      {/* Services */}
+      <Grid container className='section' />
+      {/* Contact */}
+      <Grid container className='section' />
+    </>
   )
 }
 
